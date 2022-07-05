d670aa67adb12ad98f56dcadcbcd785ba9ef626a
dediwibisono17 <dedi.wibisono17@gmail.com>
2022-07-05 15:29:55 +0700

    update credit

--- js/app.js
+++ js/app.js
@@ -37,6 +37,11 @@ $(() => {
     // $(".m-menu").click(() => {
     // });
   }
+
+  const tilt = $(".js-tilt").tilt();
+  tilt.on("change", callback); // parameters: event, transforms
+  tilt.on("tilt.mouseLeave", callback); // parameters: event
+  tilt.on("tilt.mouseEnter", callback); // parameters: event
 });
 
 //slick
